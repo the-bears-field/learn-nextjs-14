19843560e6bfcba623f63a14626d746c43dd374f
the-bears-field <thebearsfield+github@gmail.com>
2024-01-30 14:37:57 +0900

    feat: Imageコンポーネントを使用し、画像を最適化
    ルートのページにて、画像の挿入を指示されていた箇所を、Imageコンポーネントに置換。
    Tailwind CSSを使用し、レスポンシブデザインに対応。
    
    参考:
    https://nextjs.org/learn/dashboard-app/optimizing-fonts-images#why-optimize-images
    https://nextjs.org/docs/app/api-reference/components/image
    https://tailwindcss.com/docs/responsive-design

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css';
 import { lusitana } from './ui/fonts';
+import Image from 'next/image';
 
 export default function Page() {
   return (
@@ -30,7 +31,20 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
+          <Image
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            alt="デスクトップバージョンを示すダッシュボードプロジェクトのスクリーンショット"
+            className="hidden md:block"
+          />
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            alt="モバイルバージョンを示すダッシュボードプロジェクトのスクリーンショット"
+            className="block md:hidden"
+          />
         </div>
       </div>
     </main>
