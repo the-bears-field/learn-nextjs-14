bb913f252a2424a53281be33dff051c3b7f7e41c
the-bears-field <thebearsfield+github@gmail.com>
2024-02-27 14:23:28 +0900

    feat: createInvoice関数スコープ内の変数の定義方法を追加、変更
    rawFormDataを削除。
    代わりにZodObject.parseメソッドの返り値を分割代入する方法で変数を定義。
    引数はrawFormDataの定義時に使用したオブジェクトを流用。
    小数点以下の数値を排除する目的で、金額を100倍にして変数(amountInCents)に格納。
    文字列の現在の日付取得のため、Dateインスタンスを作成、toISOStringメソッドで ISO形式(ISO 8601)の文字列を取得し、splitメソッドで指定した文字列('T')で分割した配列の最初の要素を変数(date)に格納。
    
    参考:
    https://nextjs.org/learn/dashboard-app/mutating-data#4-validate-and-prepare-the-data
    https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Date
    https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/Date/toISOString
    https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/String/split

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -13,9 +13,12 @@ const FormSchema = z.object({
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(formData: FormData) {
-  const rawFormData = {
+  const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
-  };
+  });
+
+  const amountInCents: number = amount * 100;
+  const date: string = new Date().toISOString().split('T')[0];
 }
