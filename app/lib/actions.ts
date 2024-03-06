ff55018bdd3fce577bbfd1e4ff64a0e6db53b760
the-bears-field <thebearsfield+github@gmail.com>
2024-03-06 12:10:12 +0900

    feat: サーバーアクションでのスキーマ宣言時の内容を変更。
    State型を定義。
    スキーマ宣言時の内容を以下の通り変更。
    ・customerId: stringメソッドの引数に、任意のエラーメッセージを格納したオブジェクトを指定
    ・amount: gtメソッドを連結、常に0より大きい値を受け入れ、かつ任意のエラーメッセージを指定するよう変更。
    ・status: 任意のエラーメッセージを指定するよう変更。
    
    参考:
    
    https://zod.dev/?id=strings
    https://zod.dev/?id=numbers
    https://zenn.dev/tokiya_horikawa/scraps/44fd5ae47a3907
    https://zod.dev/ERROR_HANDLING?id=flattening-errors
    https://github.com/colinhacks/zod/blob/master/ERROR_HANDLING.md#flattening-errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,11 +5,27 @@ import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+// @types/react-dom が更新されるまでの暫定的な措置
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  messages?: string | null;
+};
+
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.',
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.',
+  }),
   date: z.string(),
 });
 
