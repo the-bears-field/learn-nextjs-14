85c3d2ad5a82a4c933b86f22869053b7f8ffb85e
the-bears-field <thebearsfield+github@gmail.com>
2024-03-06 17:12:51 +0900

    feat: createInvoiceアクションの処理にコメントを追加
    参考:
    https://nextjs.org/learn/dashboard-app/improving-accessibility#server-side-validation

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -33,12 +33,15 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(prevState: State, formData: FormData) {
+  // Zod を使用してフォームを検証
   const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
 
+  // フォームの検証に失敗した場合はエラーを早期に返します。
+  // それ以外の場合は続行します。
   if (!validatedFields.success) {
     return {
       errors: validatedFields.error.flatten().fieldErrors,
@@ -46,21 +49,25 @@ export async function createInvoice(prevState: State, formData: FormData) {
     };
   }
 
+  // データベースへの挿入のためのデータを準備
   const { customerId, amount, status } = validatedFields.data;
   const amountInCents: number = amount * 100;
   const date: string = new Date().toISOString().split('T')[0];
 
+  // データベースにデータを挿入
   try {
     await sql`
     INSERT INTO invoices (customer_id, amount, status, date)
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
   } catch (error) {
+    // データベースエラーが発生した場合は、より具体的なエラーを返します。
     return {
       message: 'Database Error: Failed to Create Invoice.',
     };
   }
 
+  // 請求書ページのキャッシュを再検証し、ユーザーをリダイレクトします。
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
