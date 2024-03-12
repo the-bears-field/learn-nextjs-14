7d323aeaef5aa5e1bed4bad366060c9e9e7999de
the-bears-field <thebearsfield+github@gmail.com>
2024-03-12 14:50:24 +0900

    feat: NextAuth.jsの設定ファイルに、ルートを保護するためのロジックを追加
    authConfigオブジェクトにsatisfies演算子を追加して、式が型に合致するかの検証機能を追加。
    authConfigオブジェクトに認証関連のアクションが実行時の挙動を制御するためcallbacksオプション、providersオプションを追加。
    providersオプションの値は空の配列を定義。
    callbacksオプションのオブジェクト内にauthorized関数を定義。
    引数はauthとrequestを使用。
    authorized関数のスコープ内で、以下の値を変数に格納。
    ログイン中か否かの判定結果を、authパラメータと二重否定を使用して格納。
    nextUrlから現在のパスを取得し、startsWithメソッドで指定したパスにいるかを判定し格納。
    isOnDashboard、isLoggedInの値で戻り値を変更する処理を追加。
    
    参考:
    https://nextjs.org/learn/dashboard-app/adding-authentication#protecting-your-routes-with-nextjs-middleware
    https://authjs.dev/reference/nextjs#callbacks
    https://authjs.dev/reference/nextjs#providers
    https://nextjs.org/docs/app/api-reference/functions/next-request#nexturl
    https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Operators/Logical_NOT#%E4%BA%8C%E9%87%8D%E5%90%A6%E5%AE%9A_!!
    https://developer.mozilla.org/ja/docs/Web/JavaScript/Reference/Global_Objects/String/startsWith
    https://www.typescriptlang.org/docs/handbook/release-notes/typescript-4-9.html#the-satisfies-operator
    https://jsprimer.net/basic/object/#create-object

--- auth.config.ts
+++ auth.config.ts
@@ -4,4 +4,22 @@ export const authConfig = {
   pages: {
     signIn: '/login',
   },
-};
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      const isLoggedIn: boolean = !!auth?.user;
+      const isOnDashboard: boolean = nextUrl.pathname.startsWith('/dashboard');
+
+      if (isOnDashboard) {
+        if (isLoggedIn) return true;
+        // 未認証のユーザーをログインページにリダイレクト
+        return false;
+      } else if (isLoggedIn) {
+        return Response.redirect(new URL('/dashboard', nextUrl));
+      }
+
+      return true;
+    },
+  },
+  // 現時点では空の配列を使用してプロバイダーを追加
+  providers: [],
+} satisfies NextAuthConfig;
