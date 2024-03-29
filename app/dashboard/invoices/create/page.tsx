7bffc3d8c21cc9f0e4e7ffa9c53591b8be2aba93
the-bears-field <thebearsfield+github@gmail.com>
2024-03-29 14:34:59 +0900

    feat: 各ページにメタデータを追加
    Learn Next.jsの教材で作成したページに、titleプロパティを含めたMetadata型のオブジェクトを追加。
    
    参考:
    https://nextjs.org/learn/dashboard-app/adding-metadata#practice-adding-metadata

--- app/dashboard/invoices/create/page.tsx
+++ app/dashboard/invoices/create/page.tsx
@@ -2,6 +2,11 @@ import Form from '@/app/ui/invoices/create-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers } from '@/app/lib/data';
 import { CustomerField } from '@/app/lib/definitions';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Create Invoice',
+};
 
 export default async function Page() {
   const customers: CustomerField[] = await fetchCustomers();
